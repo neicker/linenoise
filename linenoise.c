df8323101ec55dfc59efb05bbc41d68d87fc6ca0
Norbert Eicker <n.eicker@fz-juelich.de>
2016-08-02 10:43:31 +0200

    Work around offset problem in ESC[0C sequence

--- linenoise.c
+++ linenoise.c
@@ -537,7 +537,7 @@ static void refreshSingleLine(struct linenoiseState *l) {
     abAppend(&ab,seq,strlen(seq));
     /* Move cursor to original position. */
     snprintf(seq,64,"\r\x1b[%dC", (int)(pos+plen));
-    abAppend(&ab,seq,strlen(seq));
+    if (pos+plen) abAppend(&ab,seq,strlen(seq));
     if (write(fd,ab.b,ab.len) == -1) {} /* Can't recover from write error. */
     abFree(&ab);
 }
